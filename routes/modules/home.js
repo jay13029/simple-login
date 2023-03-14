13b20535c6c5182e421e4632d57ac3fabf2d18b4
jay13029 <jay13029@gmail.com>
2023-03-14 23:57:14 +0800

    Refactor routes

---
+++ routes/modules/home.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+const users = require('../../users')
+
+router.use(express.urlencoded({ extended: true }))
+
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+router.post('/', (req, res) => {
+  let check = false
+  let showMsg
+  let user = users.find(user => user.email === req.body.email)
+  if(user != null && user.password === req.body.password){
+    check = true
+    showMsg = `Welcome back, ${ user.firstName }!`
+  }
+
+  if(check){
+    res.render('welcome', { showMsg })
+  } else {
+    let message = 'Wrong email or password, please login again.'
+    res.render('index', { message })
+  }
+})
+
+module.exports = router
